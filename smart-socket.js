5800379d3ca224c03726894f5ea507f8f9e08d83
Maxime Tricoire <max.tricoire@gmail.com>
2016-09-06 17:30:50 +0200

    v0.4.7: ... must be tired

--- smart-socket.js
+++ smart-socket.js
@@ -1,31 +1,33 @@
-var WebSocket = require('ws'),
-  async = require('async'),
-  util = require('util'),
-  EventEmitter = require('events').EventEmitter;
+'use strict';
+
+var WebSocket = require('ws');
+var async = require('async');
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
 
 var noop = function () {};
 
 /**
  *
- * @param options { addresses: array, timeout: ms, loopBreak: ms, debug: boolean }
+ * @param options {object} Contains {handlers : {}, addresses: [], timeout: number, loopBreak: number, debug: boolean}
  * @constructor
  */
 var SmartSocket = function (options) {
+  this.stopped = false;
   this.id = null;
   this.wsConn = null;
-  this.stopped = false;
 
   // Handlers are DEPRECATED in favor of EventEmitter logic
-  var handlers   = options.handlers   || {};
-  this.onopen    = handlers.onopen    || noop;
-  this.onclose   = handlers.onclose   || noop;
-  this.onerror   = handlers.onerror   || noop;
+  var handlers = options.handlers || {};
+  this.onopen = handlers.onopen || noop;
+  this.onclose = handlers.onclose || noop;
+  this.onerror = handlers.onerror || noop;
   this.onmessage = handlers.onmessage || noop;
 
   this.addresses = options.addresses || [];
-  this.timeout   = options.timeout   || 10000;
+  this.timeout = options.timeout || 10000;
   this.loopBreak = options.loopBreak || 2000;
-  this.debug     = options.debug     || false;
+  this.debug = options.debug || false;
 
   // prevent default EventEmitter behavior of exiting program when 'error' event are emitted
   this.on('error', noop);
@@ -39,13 +41,43 @@ util.inherits(SmartSocket, EventEmitter);
  */
 SmartSocket.prototype.start = function () {
   var self = this;
-  var tasks = self.addresses.map(function (address) {
+  var tasks = [];
+  /**
+   * Tries to initiate a WebSocket connection with one of the given addresses
+   * Once a connection is active, the loop stops (looping is done in series = one at a time)
+   * If it is unable to connect to one of the given addresses, it will idle for this.loopBreak milliseconds
+   * and then restart the connection loop (forever, unless SmartSocket.stop() is called)
+   * @type {function(this:SmartSocket)}
+   */
+  function connectionTasks() {
+    self.emit('loopStart');
+    async.series(tasks, function (connectedWs) {
+      if (connectedWs) {
+        // successfully connected (abort connection loop)
+        self.wsConn = connectedWs;
+        self.emit('loopEnd');
+        return;
+      }
+
+      // unable to connect to any of the given server
+      if (!self.stopped) {
+        // retry connection attempt in options.loopBreak milliseconds
+        if (self.debug) {
+          console.log('Retry in ' + self.loopBreak + 'ms');
+        }
+        setTimeout(connectionTasks, self.loopBreak);
+        // notify that loop has ended
+        self.emit('loopIn', self.loopBreak);
+      }
+    });
+  }
+
+  tasks = this.addresses.map(function (address) {
     return function (cb) {
       if (self.debug) {
         console.log('Trying to connect to ws://' + address);
       }
-      var that = this;
-      that.wasOpen = false;
+
       var ws = new WebSocket('ws://' + address);
       var timeoutID = setTimeout(function () {
         try {
@@ -55,13 +87,14 @@ SmartSocket.prototype.start = function () {
           ws.close();
         } catch (ignore) {}
       }, self.timeout);
+      ws.wasOpen = false;
 
       ws.onopen = function (arg) {
         clearTimeout(timeoutID);
         if (self.debug) {
           console.log('Connection to ' + address + ' established.');
         }
-        that.wasOpen = true;
+        ws.wasOpen = true;
         self.onopen.apply(ws, [ws, arg]);
         self.emit('open', ws, arg);
         cb(ws);
@@ -85,7 +118,7 @@ SmartSocket.prototype.start = function () {
         self.onclose.apply(ws, [ws, arg]);
         self.emit('close', ws, arg);
 
-        if (that.wasOpen && !self.stopped) {
+        if (ws.wasOpen && !self.stopped) {
           connectionTasks();
         }
       };
@@ -99,36 +132,6 @@ SmartSocket.prototype.start = function () {
       };
     };
   });
-
-  /**
-   * Tries to initiate a WebSocket connection with one of the given addresses
-   * Once a connection is active, the loop stops (looping is done in series = one at a time)
-   * If it is unable to connect to one of the given addresses, it will idle for this.loopBreak milliseconds
-   * and then restart the connection loop (forever, unless SmartSocket.stop() is called)
-   * @type {function(this:SmartSocket)}
-   */
-  var connectionTasks = function () {
-    this.emit('loopStart');
-    async.series(tasks, function (connectedWs) {
-      if (connectedWs) {
-        // successfully connected (abort connection loop)
-        this.wsConn = connectedWs;
-        this.emit('loopEnd');
-        return;
-      }
-
-      // unable to connect to any of the given server
-      if (!this.stopped) {
-        // retry connection attempt in options.loopBreak milliseconds
-        if (this.debug) {
-          console.log('Retry in ' + this.loopBreak + 'ms');
-        }
-        setTimeout(connectionTasks, this.loopBreak);
-        // notify that loop has ended
-        this.emit('loopIn', this.loopBreak);
-      }
-    }.bind(this));
-  }.bind(this);
 
   process.nextTick(connectionTasks);
 };
