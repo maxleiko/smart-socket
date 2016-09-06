9ba252072106eecb22abe95b3ecdeba59c67f7a9
Maxime Tricoire <max.tricoire@gmail.com>
2016-09-06 17:39:15 +0200

    v0.4.8: fix ws shim

--- browser/ws-shim.js
+++ browser/ws-shim.js
@@ -1,17 +1,9 @@
 'use strict';
 
-var util = require('util');
-
-function WebSocketWrapper() {
-  WebSocket.call(this, arguments);
-}
-
-util.inherits(WebSocketWrapper, WebSocket);
-
-WebSocketWrapper.prototype.on = function (event, handler) {
+WebSocket.prototype.on = function (event, handler) {
   this['on' + event] = handler;
 };
-WebSocketWrapper.prototype.off = function (event, handler) {
+WebSocket.prototype.off = function (event, handler) {
   if (typeof handler === 'function') {
     if (this['on' + event] === handler) {
       delete this['on' + event];
@@ -23,5 +15,5 @@ function WebSocketServer() {
   throw new Error('WebSocketServer are not supported in browser environment.');
 }
 
-module.exports = WebSocketWrapper;
+module.exports = WebSocket;
 module.exports.WebSocketServer = WebSocketServer;
